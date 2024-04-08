d2a9a2babc6b50c12b0d24897d6da9fac5181f25
uyenntt92 <touyen810@gmail.com>
2024-04-08 20:32:45 +0700

    day code

--- src/VariablesHW/ThongTin.java
+++
@@ -1,8 +0,0 @@
-package VariablesHW;
-
-public class ThongTin {
-    public static void main(String[] args) {
-        System.out.println("Tên:" + SinhVien.ten);
-        System.out.println("Khoa:" +SinhVien.khoahoc);
-        System.out.println("Tuổi:" +SinhVien.age);
-}}
