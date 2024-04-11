fedbdc8e206129120ffd3db7fa9baa867b9496eb
uyenntt92 <touyen810@gmail.com>
2024-04-11 20:28:45 +0700

    đẩy bài tập 3

---
+++ src/ToanTu/ToanTuHW.java
@@ -0,0 +1,12 @@
+package ToanTu;
+
+public class ToanTuHW {
+           public static void main(String[] args) {
+            int a=1;
+            int b=3;
+            System.out.println("quan he lon hon:" + (b>a));
+            System.out.println("quan he nho hon bang:" + (b<a));
+            System.out.println("quan he bang:" + (b==a));
+            System.out.println("logic va:"+ ((a<3)&&(b>4)));
+            System.out.println(("logic hoac:" + ((a >= 1)||(b >= 4))));
+}}
