f0e2ef39ecd12e20cb9b5ef57f7b416c80d6a136
uyenntt92 <touyen810@gmail.com>
2024-04-11 20:32:56 +0700

    đẩy btap 1-2

---
+++ src/VariablesHW/SinhVien.java
@@ -0,0 +1,12 @@
+package VariablesHW;
+
+public class SinhVien {
+    static int khoahoc = 2023;
+    static String ten = "Ho Nhu Hoa";
+    public static int age = 20;
+    public static void main(String[] args) {
+        System.out.println("Ten: "+ ten);
+        System.out.println("tuoi:"+ age);
+        System.out.println("khoa:" + khoahoc);
+    }
+}
---
+++ src/VariablesHW/ThongTin.java
@@ -0,0 +1,8 @@
+package VariablesHW;
+
+public class ThongTin {
+    public static void main(String[] args) {
+        System.out.println("Tên:" + SinhVien.ten);
+        System.out.println("Khoa:" +SinhVien.khoahoc);
+        System.out.println("Tuổi:" +SinhVien.age);
+}}
