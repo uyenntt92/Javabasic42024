a45f4484998eaad38f05da5fda4dc61452bf0175
uyenntt92 <touyen810@gmail.com>
2024-04-14 21:07:04 +0700

    baitap3 ifelse

--- src/ToanTu/MenhDe.java
+++ src/ToanTu/MenhDe.java
@@ -1,20 +1,24 @@
-package ToanTu;
 
 public class MenhDe {
-    public static int number = 100;
-    public static int n;
-    public void SoSanh(int n) {
-        this.n = n;
+    static  int number = 100;
+
+    // Hàm kiểm tra số n
+    static void checkNumber(int n) {
+        if (n == number) {
+            System.out.println("n bằng " + number);
+        } else if (n < number) {
+            System.out.println("n nhỏ hơn " + number);
+        } else {
+            System.out.println("n lớn hơn " + number);
+        }
     }
     public static void main(String[] args) {
-        MenhDe ThamSo = new  MenhDe();
-        ThamSo.SoSanh(25);
-
-        if (n == number) {
-            System.out.println("n bang 100");
-        } else if (n < number) {
-            System.out.println("n nho hon 100");
-        } else {
-            System.out.println("n lon hon 100");
-        }
-    }}
+        // Gọi hàm để kiểm tra các trường hợp
+        // TH1: n == number
+        checkNumber(100);
+        // TH2: n < number
+        checkNumber(50);
+        // TH3: n > number
+        checkNumber(150);
+    }
+}
