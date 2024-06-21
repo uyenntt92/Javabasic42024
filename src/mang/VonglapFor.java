b18618854903259ed0b49b82a3758906006c5b2f
uyenntt92 <166031736+uyenntt92@users.noreply.github.com>
2024-06-21 20:50:07 +0700

    Add files via upload

---
+++ src/mang/VonglapFor.java
@@ -0,0 +1,10 @@
+package mang;
+public class VonglapFor {
+    public static void main(String[] args) {
+        for (int a= 0; a<= 50;a++ ){
+            if ( a%2==0){
+                System.out.println(a);
+            }
+        }
+    }
+}
