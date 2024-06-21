eba371071c5ebb16b0030476ca08f5cc585416cb
uyenntt92 <166031736+uyenntt92@users.noreply.github.com>
2024-06-21 20:48:46 +0700

    Add files via upload

---
+++ VarriableHW/ThongTin.java
@@ -0,0 +1,9 @@
+package VarriableHW;
+
+public class ThongTin {
+            public static void main(String[] args) {
+            System.out.println("Tên:" + SinhVien.ten);
+            System.out.println("Khoa:" +SinhVien.khoahoc);
+            System.out.println("Tuổi:" +SinhVien.age);
+        }
+}
