993e8a43ccc0ceab996411fbd10d446476b6c161
uyenntt92 <166031736+uyenntt92@users.noreply.github.com>
2024-06-21 20:51:29 +0700

    Delete VarriableHW directory

--- VarriableHW/ThongTin.java
+++
@@ -1,9 +0,0 @@
-package VarriableHW;
-
-public class ThongTin {
-            public static void main(String[] args) {
-            System.out.println("Tên:" + SinhVien.ten);
-            System.out.println("Khoa:" +SinhVien.khoahoc);
-            System.out.println("Tuổi:" +SinhVien.age);
-        }
-}
